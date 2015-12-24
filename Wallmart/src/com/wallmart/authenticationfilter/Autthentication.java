1a4f052fbfed4958d43a4d11e696949ff68cebec
dilipbhatt <dilip.kumar@niletechnologies.com>
2015-12-24 18:53:59 +0530

    Commit with Jar
    Commit with Jar

--- Wallmart/src/com/wallmart/authenticationfilter/Autthentication.java
+++ Wallmart/src/com/wallmart/authenticationfilter/Autthentication.java
@@ -1,5 +1,7 @@
 package com.wallmart.authenticationfilter;
 
+import java.sql.ResultSet;
+
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 import com.wallmart.dao.WallmartDBConnection;
@@ -7,14 +9,18 @@ import com.wallmart.dto.WallmartDTO;
 
 public class Autthentication {
 	private Connection connection;
-	String QUERY_PART1 = "SELECT USER_NAME, USER_ID, PASSWORD FROM  USER_CREDENTIAL  WHERE"; 
-	//String QUERY_PART2 =
+	//String QUERY_PART1 = "SELECT USER_NAME, USER_ID, PASSWORD FROM  USER_CREDENTIAL  WHERE"; 
+	String QUERY_PART1 = "SELECT USER_NAME, USER_ID, PASSWORD FROM USER_CREDENTIAL" + " WHERE USER_ID >= 101 ";
+	
 	public void Autthentication(){
 		System.out.println("---------------- In Autthentication --------------");
 		
 		
 		connection = (Connection)WallmartDBConnection.getConnection();
-	//	 Statement stmt = connection.createStatement();
+		
+		//	 Statement stmt = (Statement)connection.createStatement();
+		
+
      //    ResultSet totalCountRS = stmt.executeQuery("select count(*) from MIGRATION_TEST1_BKP");
 	}
 public static boolean userAuthentication(String userID, String password){
@@ -26,6 +32,8 @@ public static boolean userAuthentication(String userID, String password){
 	if(userCredential_ID.equalsIgnoreCase(userID) && userCredential_Password.equals(password)){
 		System.out.println("--------- Valid userID --------"+userID);
 		System.out.println("--------- Valid userID --------"+password);
+		// ResultSet rset = stmt.executeQuery("SELECT USER_NAME, USER_ID, PASSWORD FROM " + "USER_CREDENTIAL" + " WHERE USER_ID = "  + "'"+currentData1+"'" +  " AND " +"'"+currentData2+"'");
+			
 		validation_flag = true;
 	}
 	else{
